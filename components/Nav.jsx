ab081a84c0e47ecafcb53b8be26ee156f0432ac3
abdourahman <aj22018057@utg.edu.gm>
2023-07-03 11:31:16 +0000

    Removed env file

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,6 +6,8 @@ import { useState, useEffect } from 'react';
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 function Nav() {
+    const isUserLoggedIn = true;
+
     return (
         <nav className='flex-between pt-3 mb-16 w-full'>
             <Link href='/' className='flex gap-2 flex-center'>
@@ -18,6 +20,19 @@ function Nav() {
                 />
                 <p className='logo_text'>Promptopia</p>
             </Link>
+
+            {/* Mobile Navigation */}
+            <div className='sm:flex hidden'>
+                {isUserLoggedIn ? (
+                    <div className='flex gap-3 md:gap-5'>
+                        <Link href='/create-prompt' className='black_btn'>
+                            Create prompt
+                        </Link>
+                    </div>
+                ) : (
+                    <></>
+                )}
+            </div>
         </nav>
     );
 }
